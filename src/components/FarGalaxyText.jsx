61e0bb1a2bb2e4ef1696392458e398c0a5f89d4e
GulnaraBykhovskaya <bykhovskaya.buro@gmail.com>
2025-01-20 17:35:47 +0200

    Added spiner and enabled the download of openning crawl

--- src/components/FarGalaxyText.jsx
+++ src/components/FarGalaxyText.jsx
@@ -1,9 +1,32 @@
-import { openingCrawl } from '../utils/constants.js'
+import { baseUrl } from '../utils/constants.js';
+import { useEffect, useState } from 'react';
 
 const FarGalaxyText = () => {
-  return (
-    <p className="farGalaxy">{openingCrawl}</p>
-  );
+  const [openingCrawl, setOpeningCrawl] = useState('');
+  const [spinnerClass, setSpinnerClass] = useState('spinner-border');
+
+  const getRandomSeason = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
+
+  const getOpeningCrawl = () => {
+    const randomSeason = getRandomSeason(1, 6);
+    const url = `${baseUrl}${randomSeason}`;
+    console.log(url);
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setOpeningCrawl(data.opening_crawl);
+        setSpinnerClass('');
+      })
+      .catch((error) => console.error('Error:', error));
+  };
+
+  useEffect(() => {
+    getOpeningCrawl();
+  }, []);
+
+  return <p className={`farGalaxy ${spinnerClass}`}>{openingCrawl}</p>;
 };
 
 export default FarGalaxyText;
