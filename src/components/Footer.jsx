d4c2a22ea7ae2c42cd7a70976d2f0b12983dbd39
GulnaraBykhovskaya <bykhovskaya.buro@gmail.com>
2025-01-27 05:27:47 +0200

    installed Tailwind in the project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
 const footer = () => {
   return (
-    <footer className="rounded-bottom-4 row align-items-center">
-      <div className="btn btn-danger col-2 offset-4">Send me an <span
-        className="text-black text-uppercase">email</span></div>
+    <footer className='bg-grey-color rounded-bottom-4 row align-items-center'>
+      <div className='btn btn-danger col-2 offset-4'>
+        Send me an <span className='text-black text-uppercase'>email</span>
+      </div>
     </footer>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import Navigation from './Navigation.jsx'
-const header = ({changePage}) => {
+import Navigation from './Navigation.jsx';
+const header = ({ changePage }) => {
   return (
-      <header className="rounded-top-4">
-        <Navigation changePage={changePage}/>
-        <h1 className="text-center fs-1 py-3">Luke Skywalker</h1>
-      </header>
+    <header className='rounded-t-2xl bg-grey-color'>
+      <Navigation changePage={changePage} />
+      <h1 className='text-center fs-1 py-3'>Luke Skywalker</h1>
+    </header>
   );
 };
 
